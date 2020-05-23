91a82d5e4c314765d5ff824adeab5c488206e958
imifos <chief@imifos.be>
2020-05-23 15:44:04 +0200

    .

--- README.md
+++ README.md
@@ -1,11 +1,16 @@
 # USSM - Ultra-Simple Stock Monitor
 
-Small fancy afternoon'ish-project Python script that monitors a list of stock quotes. 
+Small'n fancy afternoon'ish-project Python script that monitors a list of stock quotes. 
+Focus is on long-term behaviour and not short term-fluctuations.
+
 Displays 5y,2y and 3mo price history, auto-circles over all quotes every 15 seconds, 
-downloads the data from YAHOO every hour. Uses the tkinter (Tk interface), Matplotlib 
-and the yahooquery package, which have to be installed. 
-Configuration at the top of the script itself. Tickers loaded from text file passed 
-to the script, one ticker per line.
+downloads the data from YAHOO every hour. Click on the symbol in the left-handle table displays this 
+plot. Clicking in the plot suspends the auto-advance for a minute.
+
+Configuration at the top of the script itself.
+Tickers loaded from text file passed to the script, one ticker per line.
+ 
+Uses the tkinter (Tk interface), Matplotlib and the yahooquery package, which have to be installed. 
 
 ![Screenshot](https://raw.githubusercontent.com/imifos/us-stockmonitor/master/ussm.jpg)
 
--- stock-monitor.py
+++ stock-monitor.py
@@ -10,13 +10,15 @@ by @imifos
 05/2020
 GPL3
 """
-import tkinter as tk
-import matplotlib.pyplot as plt
-from yahooquery import Ticker
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import datetime
+import tkinter as tk
+
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from yahooquery import Ticker
 
 AUTO_UPDATE_TIMER = 10
+
 
 #
 #
@@ -27,22 +29,22 @@ class StockSymbolTable(tk.Frame):
     """
 
     def __init__(self, parent, rows):
-        columns=3
+        columns = 3
         tk.Frame.__init__(self, parent, background="gray")
         self._widgets = []
         for row in range(rows):
             current_row = []
+            self._widgets.append(current_row)
             for column in range(columns):
                 w = 8
-                f='Helvetica 12'
+                f = 'Helvetica 12'
                 c = "left_side"
-                if column==0: f+=' bold'
-                if column==0: c="center_ptr"
-                if column==1: w=20
-                label = tk.Label(self, fg="black",bg="white",text="", borderwidth=0, width=w,font=f,cursor=c)
+                if column == 0: f += ' bold'
+                if column == 0: c = "center_ptr"
+                if column == 1: w = 20
+                label = tk.Label(self, fg="black", bg="white", text="", borderwidth=0, width=w, font=f, cursor=c)
                 label.grid(row=row, column=column, sticky="nsew", padx=1, pady=1)
                 current_row.append(label)
-            self._widgets.append(current_row)
 
         for column in range(columns):
             self.grid_columnconfigure(column, weight=1)
@@ -64,25 +66,25 @@ class StockSymbolTable(tk.Frame):
 class DataModel:
     """ Handles all aspects of stock data, including caching and querying the YAHOO web service. """
 
-    data_cache_time={}
-    data_df2y_cache={}
-    data_df5y_cache ={}
-    data_df3m_cache ={}
-    data_price_cache ={}
-    short_name_cache={}
+    data_cache_time = {}
+    data_df2y_cache = {}
+    data_df5y_cache = {}
+    data_df3m_cache = {}
+    data_price_cache = {}
+    short_name_cache = {}
 
     def get_ticker_data(self, symbol):
         """Returns the stock symbol ticker data, either from YAHOO or from local cache."""
         now = datetime.datetime.now()
-        x=self.data_df2y_cache.get(symbol, None)
-        cache_time=self.data_cache_time.get(symbol,99)
-        if cache_time!=now.hour or x is None:
+        x = self.data_df2y_cache.get(symbol, None)
+        cache_time = self.data_cache_time.get(symbol, 99)
+        if cache_time != now.hour or x is None:
 
             print("download")
-            self.data_cache_time[symbol]=now.hour
+            self.data_cache_time[symbol] = now.hour
 
             t = Ticker(symbol, formatted=True)
-            valid=t.validation[list(t.validation)[0]]
+            valid = t.validation[list(t.validation)[0]]
             if valid:
                 df2y = t.history(period='2y', interval='1d')
                 df5y = t.history(period='5y', interval='5d')
@@ -90,16 +92,16 @@ class DataModel:
                 price = float(t.price[list(t.price)[0]]['regularMarketPrice']['fmt'])
                 short_name = t.quote_type[list(t.quote_type)[0]]['shortName']
             else:
-                df2y=None
-                df5y=None
-                df3m=None
-                price=0
-                short_name="Unknown Ticker"
+                df2y = None
+                df5y = None
+                df3m = None
+                price = 0
+                short_name = "Unknown Ticker"
 
-            self.data_df2y_cache[symbol]=df2y
-            self.data_df5y_cache[symbol]=df5y
-            self.data_df3m_cache[symbol]=df3m
-            self.data_price_cache[symbol]=price
+            self.data_df2y_cache[symbol] = df2y
+            self.data_df5y_cache[symbol] = df5y
+            self.data_df3m_cache[symbol] = df3m
+            self.data_price_cache[symbol] = price
             self.short_name_cache[symbol] = short_name
         else:
             df2y = self.data_df2y_cache.get(symbol)
@@ -110,7 +112,7 @@ class DataModel:
 
         return float(price), df3m, df5y, df2y
 
-    def get_short_name(self,symbol):
+    def get_short_name(self, symbol):
         """Returns the name of the company by its ticker name, if already downloaded. The ticker name gets downloaded
         with it stock history data, so it's coming in deferred. """
         return self.short_name_cache.get(symbol, "")
@@ -118,8 +120,7 @@ class DataModel:
     def get_price(self, symbol):
         """Returns the price of the share by its ticker name, if already downloaded. The price gets downloaded
         with it stock history data, so it's coming in deferred. """
-        return self.data_price_cache.get(symbol,0)
-
+        return self.data_price_cache.get(symbol, 0)
 
 
 #
@@ -135,7 +136,7 @@ class App(tk.Tk):
     TICKERS = None
     DATA = DataModel()
 
-    def __init__(self,tickers):
+    def __init__(self, tickers):
         "Setup"
         tk.Tk.__init__(self)
 
@@ -147,7 +148,7 @@ class App(tk.Tk):
         self.datatable = StockSymbolTable(self, len(self.TICKERS))
         self.datatable.pack(side=tk.LEFT, anchor=tk.NW)
 
-        for index,c in enumerate(self.TICKERS):
+        for index, c in enumerate(self.TICKERS):
             self.datatable.set(0, index, c).bind_click(0, index, self.on_table_click)
             self.datatable.bind_click(1, index, self.on_table_click)
 
@@ -159,34 +160,35 @@ class App(tk.Tk):
         self.configure(background='white')
         self.title("USSM - Ultra-Simple Stock Monitor")
         self.geometry("1500x1000")
+        #self.iconbitmap("xx.ico")
+
         self.after(self.INTERVAL, self.timer_signal)
 
     def on_table_click(self, event):
         """Click on stock symbol in table to display selected graph. """
         click_text = event.widget.cget("text")
-        for ndx,symbol in enumerate(self.TICKERS):
+        for ndx, symbol in enumerate(self.TICKERS):
             if symbol == click_text:
                 if ndx != self.current_symbol_index:
                     self.current_symbol_index = ndx
                     self.update_graph()
-                    self.skip_next_update_timer_update = 5 # suspend auto-advance for a few intervals
+                    self.skip_next_update_timer_update = 5  # suspend auto-advance for a few intervals
                     break
 
     def on_graph_click(self, event):
         """Click on stock graph. """
-        self.skip_next_update_timer_update = 5 # suspend auto-advance for a few intervals
+        self.skip_next_update_timer_update = 5  # suspend auto-advance for a few intervals
 
     def timer_signal(self):
         """Auto-advance forward timer signal received. """
-        if self.skip_next_update_timer_update>0:
-            self.skip_next_update_timer_update-=1
+        if self.skip_next_update_timer_update > 0:
+            self.skip_next_update_timer_update -= 1
         else:
             self.current_symbol_index += 1
             if self.current_symbol_index >= len(self.TICKERS):
                 self.current_symbol_index = 0
             self.update_graph()
         self.after(self.INTERVAL, self.timer_signal)
-
 
     def update_graph(self):
         """ Replace current graph with new one specified by the current symbol index. """
@@ -196,7 +198,7 @@ class App(tk.Tk):
             plt.close(self.graph_canvas.figure)
             self.graph_canvas.get_tk_widget().destroy()
 
-        self.graph_canvas=tmp_canvas
+        self.graph_canvas = tmp_canvas
         self.graph_canvas.get_tk_widget().pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
         self.graph_canvas.get_tk_widget().bind("<Button-1>", self.on_graph_click)
 
@@ -212,9 +214,12 @@ class App(tk.Tk):
         """Gets stock data from the data manager and constructs a TKINTER canvas with plot. """
 
         fig = plt.figure(figsize=(10, 10), dpi=100)
-        canvas=FigureCanvasTkAgg(fig, master=self)
+        canvas = FigureCanvasTkAgg(fig, master=self)
+        canvas.get_tk_widget().config(cursor="center_ptr")
 
-        price,df3m,df5y,df2y = self.DATA.get_ticker_data(self.get_current_symbol())
+        price, df3m, df5y, df2y = self.DATA.get_ticker_data(self.get_current_symbol())
+
+        # No data, likely due to a ticker typo or network error
         if df3m is None:
             return canvas
 
@@ -222,37 +227,38 @@ class App(tk.Tk):
         ax1 = fig.add_subplot(111, label="2y")
         ax1.plot(df2y['high'], color='darkgreen', linewidth=1)
         ax1.set_xlabel("2 Years", color="darkgreen", fontsize=8)
-        ax1.xaxis.set_label_coords(0.2, 1.015)
-        #ax1.yaxis.set_visible(False)
-        ax1.set_title(self.get_current_symbol()+" - "+self.DATA.get_short_name(self.get_current_symbol()))
-        ax1.axhline(y=price,linewidth=1, color='darkgreen',linestyle=':')
+        ax1.xaxis.set_label_coords(0.10, 1.015)
+        # ax1.yaxis.set_visible(False)
+        ax1.axhline(y=price, linewidth=1, color='darkgreen', linestyle=':')
         ax1.margins(0.01)
 
-        ax1.text(0.5, 0.5, price, transform=ax1.transAxes, fontsize=40, color='lightgray', alpha=0.7, ha='center',
-                 va='center', rotation='0')
+        ax1.set_title(self.get_current_symbol() + " - " + str(price) + " - " + self.DATA.get_short_name(
+            self.get_current_symbol()))
+        # ax1.text(0.5, 0.5, price, transform=ax1.transAxes, fontsize=40, color='lightgray', alpha=0.7, ha='left',va='center', rotation='0')
 
         ax2 = fig.add_subplot(111, label="5y", frame_on=False)
-        ax2.plot(df5y['high'], color='peachpuff', linestyle=':')
-        ax2.set_xlabel("5 Years", color="peachpuff", fontsize=8)
+        ax2.plot(df5y['high'], color='coral', linestyle=':')
+        ax2.set_xlabel("5 Years", color="coral", fontsize=8)
         ax2.xaxis.set_label_coords(0.15, 1.015)
         ax2.margins(0.01)
 
         ax3 = fig.add_subplot(111, label="3mo", frame_on=False)
-        ax3.plot(df3m['high'], color='skyblue', linewidth=1, linestyle=':')
-        ax3.set_xlabel("3 Months", color="skyblue", fontsize=8)
-        ax3.xaxis.set_label_coords(0.1, 1.015)
-        #ax3.xaxis.set_ticks_position('none')
+        ax3.plot(df3m['high'], color='lightgray', linewidth=1, linestyle=':')
+        ax3.set_xlabel("3 Months", color="lightgray", fontsize=8)
+        ax3.xaxis.set_label_coords(0.04, 1.015)
+        # ax3.xaxis.set_ticks_position('none')
         ax3.margins(0.01)
 
-        plt.setp(ax3.xaxis.get_majorticklabels(), rotation=-90, fontsize=6, color='skyblue')
-        plt.setp(ax2.xaxis.get_majorticklabels(), rotation=-90, fontsize=6, color='peachpuff')
+        plt.setp(ax3.xaxis.get_majorticklabels(), rotation=-90, fontsize=6, color='lightgray')
+        plt.setp(ax2.xaxis.get_majorticklabels(), rotation=-90, fontsize=6, color='coral')
         plt.setp(ax1.xaxis.get_majorticklabels(), rotation=-90, fontsize=9, color='darkgreen')
-        plt.setp(ax3.yaxis.get_majorticklabels(),  fontsize=6, color='skyblue')
-        plt.setp(ax2.yaxis.get_majorticklabels(),  fontsize=6, color='peachpuff')
+        plt.setp(ax3.yaxis.get_majorticklabels(), fontsize=6, color='lightgray')
+        plt.setp(ax2.yaxis.get_majorticklabels(), fontsize=6, color='coral')
         plt.setp(ax1.yaxis.get_majorticklabels(), fontsize=9, color='darkgreen')
-        #plt.autoscale(tight=True)
+        plt.autoscale(tight=True)
 
         return canvas
+
 
 #
 #
@@ -261,11 +267,11 @@ if __name__ == "__main__":
 
     import sys
 
-    if len(sys.argv)<2:
+    if len(sys.argv) < 2:
         print("Please pass a file. 1 stock ticker symbol per line.")
         exit(-1)
 
-    tickers=[]
+    tickers = []
     with open(sys.argv[1]) as fp:
         line = fp.readline()
         while line:
